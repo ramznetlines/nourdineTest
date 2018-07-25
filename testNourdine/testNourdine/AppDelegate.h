e86b103c0ce5aa804f3b132d0c2a8222fcd6cd5b
RAMZ Mohamed Rabia <mrramz@ippon.fr>
2018-07-25 20:08:22 +0100

    first commit2

---
+++ testNourdine/testNourdine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testNourdine
+//
+//  Created by RAMZ on 7/25/18.
+//  Copyright © 2018 RAMZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testNourdine/testNourdine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testNourdine
+//
+//  Created by RAMZ on 7/25/18.
+//  Copyright © 2018 RAMZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
